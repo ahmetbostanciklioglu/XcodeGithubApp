6258c2a5624951c5cc5a6c951793c55eeec60376
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-03-10 19:32:49 +0300

    Initial Commit

---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by Ahmet Bostancıklıoğlu on 10.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
